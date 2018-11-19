44cdac1a4fb58a49be93b9f1799fd1b2b486cc12
Zhanibek Nagashybay <35088721+drradio@users.noreply.github.com>
2018-11-19 21:01:50 +0600

    Add files via upload
    * Improved decode function;
    * Added config options support;

--- xmp/xmp.c
+++ xmp/xmp.c
@@ -41,24 +41,59 @@
 #include "options.h"
 
 /* Hardcoded options */
-#ifndef OUTPUTRATE
-#define OUTPUTRATE 44100
+#ifndef _XMP_MIXRATE
+#define _XMP_MIXRATE 48000
 #endif
 
 typedef struct
 {
-    xmp_context context;
-    int length;
     struct decoder_error error;
+    xmp_context context;
+    int   duration;
+    char *buffer;
+    int   bufferSize;
+    int   consumed;
+    bool  isEnd;
+    int   rate;
+    int   format; // Output format
 } xmp_data_t;
 
+/* See http://xmp.sourceforge.net/libxmp.html#player-parameter-setting for details */
+/* Config option: mixing rate */
+static int xmp_mixrate = _XMP_MIXRATE;
 
-static xmp_data_t *loadmod (const char *uri)
+/* Config option: mixing format: 0 == 16 bit stereo */
+static int xmp_format = 0;
+
+/* Config option: interpolation type: nearest, linear and spline */
+static int xmp_interpolation = XMP_INTERP_SPLINE;
+
+/* Config option: stereo separation in percent: default is 70 */
+static int xmp_separation = 70;
+
+/* Config option: DSP effects: all, lowpass */
+static int xmp_dsp_effects = XMP_DSP_ALL;
+
+/* Config option: player flags: vblank, fx9bug, fixloop, a500 */
+static int xmp_flags = 0;
+
+/* Config option: default panning separation in formats with only left and right: default is 100 */
+static int xmp_default_pan = 100;
+
+/* Config option: player mode, emulate specific tracker:
+ * auto, mod, noisetracker, protracker, s3m, st3, st3gus, xm, ft2, it, itsmp */
+static int xmp_mode = XMP_MODE_AUTO;
+
+/* Config option: maximum number of voices: default is 128 */
+static int xmp_voices = 128;
+
+
+static xmp_data_t *_xmp_load (const char *uri)
 {
     xmp_data_t *mod = xmalloc (sizeof(xmp_data_t));
 
-    mod->context = xmp_create_context();
-    mod->length  = 0;
+    mod->context  = xmp_create_context();
+    mod->duration = 0;
     decoder_error_init (&mod->error);
 
     struct io_stream *s = io_open (uri, 0);
@@ -109,27 +144,40 @@ static xmp_data_t *loadmod (const char *uri)
     return mod;
 }
 
-static void *__open (const char *uri)
+static void *_xmp_open (const char *uri)
 {
-    xmp_data_t *mod = loadmod (uri);
+    xmp_data_t *mod = _xmp_load (uri);
 
     if (mod->error.type != ERROR_OK)
         return mod;
 
     /* Start player with maximal quality options */
-    xmp_start_player (mod->context, OUTPUTRATE, 0);
+    xmp_start_player (mod->context, xmp_mixrate,       xmp_format);
+    xmp_set_player   (mod->context, XMP_PLAYER_MIX,    xmp_separation);
+    xmp_set_player   (mod->context, XMP_PLAYER_INTERP, xmp_interpolation);
+    xmp_set_player   (mod->context, XMP_PLAYER_DSP,    xmp_dsp_effects);
+    xmp_set_player   (mod->context, XMP_PLAYER_FLAGS,  xmp_flags);
+    xmp_set_player   (mod->context, XMP_PLAYER_DEFPAN, xmp_default_pan);
+    xmp_set_player   (mod->context, XMP_PLAYER_MODE,   xmp_mode);
+    xmp_set_player   (mod->context, XMP_PLAYER_VOICES, xmp_voices);
 
     struct xmp_module_info info;
 
     xmp_get_module_info (mod->context, &info);
 
-    /* I don't know why do we need add 0.5 sec to length but it works */
-    mod->length = (info.seq_data[0].duration + 500) / 1000;
+    /* I don't know why do we need add 0.5 sec to duration but it works */
+    mod->duration   = (info.seq_data[0].duration + 500) / 1000;
+    mod->isEnd      = false;
+    mod->consumed   = 0;
+    mod->buffer     = NULL;
+    mod->bufferSize = 0;
+    mod->format     = xmp_format;
+    mod->rate       = xmp_mixrate;
 
     return mod;
 }
 
-static void __close (void *data)
+static void _xmp_close (void *data)
 {
     xmp_data_t *mod = data;
     decoder_error_clear(&mod->error);
@@ -138,30 +186,78 @@ static void __close (void *data)
     xmp_free_context    (mod->context);
 }
 
-static int __decode (void *data, char *buf, int len, struct sound_params *sound_params)
+/* Based on function xmp_play_buffer() by Claudio Matsuoka and Hipolito Carraro Jr */
+static int _xmp_decode (void *data, char *buffer, int size, struct sound_params *sound_params)
 {
-    sound_params->channels = 2;
-    sound_params->rate = OUTPUTRATE;
-    sound_params->fmt = SFMT_S16 | SFMT_NE;
-    int ret = xmp_play_buffer (((xmp_data_t*)data)->context, buf, len, 1);
-    return ret == -XMP_END? 0: len;
+    xmp_data_t* mod = data;
+    int ret = 0, filled = 0, copySize;
+
+    sound_params->channels = mod->format & XMP_FORMAT_MONO? 1: 2;
+    sound_params->fmt      = mod->format & XMP_FORMAT_8BIT? SFMT_S8: SFMT_S16 | SFMT_NE;
+    sound_params->rate     = mod->rate;
+
+    if (mod->isEnd)
+        return 0;
+
+    /* Fill buffer */
+    while (filled < size)
+    {
+        /* Check if buffer full */
+        if (mod->consumed == mod->bufferSize)
+        {
+            struct xmp_frame_info info;
+
+            ret = xmp_play_frame (mod->context);
+            xmp_get_frame_info (mod->context, &info);
+
+            /* Check end of module */
+            if (ret < 0 || info.loop_count >= 1)
+            {
+                mod->isEnd = true;
+
+                /* Start of frame, return end of replay */
+                if (!filled)
+                {
+                    mod->consumed   = 0;
+                    mod->bufferSize = 0;
+                    return 0;
+                }
+
+                /* Fill remaining of this buffer */
+                memset (buffer + filled, 0, size - filled);
+                return filled;
+            }
+
+            mod->consumed   = 0;
+            mod->buffer     = info.buffer;
+            mod->bufferSize = info.buffer_size;
+        }
+
+        /* Copy frame data to user buffer */
+        copySize = MIN(size - filled, mod->bufferSize - mod->consumed);
+        memcpy (buffer + filled, mod->buffer + mod->consumed, copySize);
+        mod->consumed += copySize;
+        filled += copySize;
+    }
+
+    return size;
 }
 
-static int __seek (void *data, int sec)
+static int _xmp_seek (void *data, int sec)
 {
     xmp_data_t *mod = data;
 
-    sec = MIN(sec, mod->length);
+    sec = MIN(sec, mod->duration);
 
-    if (xmp_seek_time (mod->context, sec * 1000 - 500) == -XMP_ERROR_STATE)
+    if (xmp_seek_time (mod->context, sec * 1000 - 500) < 0)
         return -1;
 
     return sec;
 }
 
-static void __info (const char *uri, struct file_tags *tags, const int selected)
+static void _xmp_info (const char *uri, struct file_tags *tags, const int selected)
 {
-    xmp_data_t *mod = loadmod (uri);
+    xmp_data_t *mod = _xmp_load (uri);
 
     if (mod->error.type != ERROR_OK)
         return;
@@ -182,27 +278,27 @@ static void __info (const char *uri, struct file_tags *tags, const int selected)
         tags->filled |= TAGS_COMMENTS;
     }
 
-    __close (mod);
+    _xmp_close (mod);
 }
 
 /* Modules don't have bitrate */
-static int __get_bitrate (void *data ATTR_UNUSED)
+static int _xmp_get_bitrate (void *data ATTR_UNUSED)
 {
     return -1;
 }
 
-static int __get_duration (void *data)
+static int _xmp_get_duration (void *data)
 {
-    return ((xmp_data_t*)data)->length;
+    return ((xmp_data_t*)data)->duration;
 }
 
-static void __get_error (void *data, struct decoder_error *error)
+static void _xmp_get_error (void *data, struct decoder_error *error)
 {
     decoder_error_copy (error, &((xmp_data_t*)data)->error);
 }
 
 /* TODO: add more supported extensions */
-static int __our_format_ext (const char *ext)
+static int _xmp_our_format_ext (const char *ext)
 {
     return !strcasecmp (ext, "NONE") ||
            !strcasecmp (ext, "XM")   ||
@@ -241,17 +337,17 @@ static struct decoder xmp_decoder =
     DECODER_API_VERSION,
     NULL,
     NULL,
-    __open,
+    _xmp_open,
     NULL,
     NULL,
-    __close,
-    __decode,
-    __seek,
-    __info,
-    __get_bitrate,
-    __get_duration,
-    __get_error,
-    __our_format_ext,
+    _xmp_close,
+    _xmp_decode,
+    _xmp_seek,
+    _xmp_info,
+    _xmp_get_bitrate,
+    _xmp_get_duration,
+    _xmp_get_error,
+    _xmp_our_format_ext,
     NULL,
     NULL,
     NULL,
@@ -259,7 +355,79 @@ static struct decoder xmp_decoder =
     NULL
 };
 
+static struct playermode
+{
+    char *string;
+    int   value;
+} playermodes[] =
+{
+    {"AUTO",         XMP_MODE_AUTO},
+    {"MOD",          XMP_MODE_MOD},
+    {"NOISETRACKER", XMP_MODE_NOISETRACKER},
+    {"PROTRACKER",   XMP_MODE_PROTRACKER},
+    {"S3M",          XMP_MODE_S3M},
+    {"ST3",          XMP_MODE_ST3},
+    {"ST3GUS",       XMP_MODE_ST3GUS},
+    {"XM",           XMP_MODE_XM},
+    {"FT2",          XMP_MODE_FT2},
+    {"IT",           XMP_MODE_IT},
+    {"ITSMP",        XMP_MODE_ITSMP},
+};
+
 struct decoder *plugin_init()
 {
+    char *value = NULL;
+    size_t i;
+
+    /* Get numeric options */
+    xmp_mixrate     = options_get_int ("XMP_MixingRate");
+    xmp_voices      = options_get_int ("XMP_Voices");
+    xmp_separation  = options_get_int ("XMP_StereoSeparation");
+    xmp_default_pan = options_get_int ("XMP_DefaultPan");
+
+    if (options_get_bool ("XMP_8bit"))
+        xmp_format |= XMP_FORMAT_8BIT;
+
+    if (options_get_bool ("XMP_Mono"))
+        xmp_format |= XMP_FORMAT_MONO;
+
+    /* Get interpolation mode */
+    value = options_get_symb ("XMP_Interpolation");
+    if (!strcasecmp(value, "NEAREST"))
+        xmp_interpolation = XMP_INTERP_NEAREST;
+    else if (!strcasecmp(value, "LINEAR"))
+        xmp_interpolation = XMP_INTERP_LINEAR;
+    else if (!strcasecmp(value, "SPLINE"))
+        xmp_interpolation = XMP_INTERP_SPLINE;
+
+    /* Get DSP effects mode */
+    value = options_get_symb ("XMP_DSPEffects");
+    if (!strcasecmp(value, "ALL"))
+        xmp_dsp_effects = XMP_DSP_ALL;
+    else if (!strcasecmp(value, "LOWPASS"))
+        xmp_dsp_effects = XMP_DSP_LOWPASS;
+
+    /* Get player mode */
+    value = options_get_symb ("XMP_PlayerMode");
+    for (i = 0; i < sizeof(playermodes)/sizeof(struct playermode); i++)
+    {
+        if (!strcasecmp(value, playermodes[i].string))
+        {
+            xmp_mode = playermodes[i].value;
+            break;
+        }
+    }
+
+    /* Get player flags */
+    lists_t_strs *list = options_get_list ("XMP_PlayerFlags");
+    if (lists_strs_exists(list, "VBLANK"))
+        xmp_flags |= XMP_FLAGS_VBLANK;
+    if (lists_strs_exists(list, "FX9BUG"))
+        xmp_flags |= XMP_FLAGS_FX9BUG;
+    if (lists_strs_exists(list, "FIXLOOP"))
+        xmp_flags |= XMP_FLAGS_FIXLOOP;
+    if (lists_strs_exists(list, "A500"))
+        xmp_flags |= XMP_FLAGS_A500;
+
     return &xmp_decoder;
 }
